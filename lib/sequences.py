9fbb3e98c3589309c46f8616d5999ef83c119365
william <williamwambugu663@gmail.com>
2025-05-14 13:50:05 +0300

    Initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
+    fib_sequence = []
+
+    for i in range(length):
+        if i == 0:
+            fib_sequence.append(0)
+        elif i == 1:
+            fib_sequence.append(1)
+        else:
+            fib_sequence.append(fib_sequence[i - 1] + fib_sequence[i - 2])
+    print(fib_sequence)
     pass
